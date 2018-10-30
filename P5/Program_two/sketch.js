58d48412e58afc8a9dd511a805f583685b17ad81
Tyler <tstarr@cocc.edu>
2018-10-30 16:48:20 -0700

    added p5

---
+++ P5/Program_two/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  // put setup code here
+  createCanvas(800,800);
+}
+
+function draw() {
+  // put drawing code here
+  if(mouseIsPressed) 
+    {
+      fill(0);
+    }
+    else {
+      fill(255);
+    }
+    ellipse(mouseX,mouseY,80,80);
+}
